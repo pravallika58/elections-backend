0891fdd5b2fafe92f7d548ac0f3c1b0bf75a3ec7
Hamza Sharif Butt <Hamzasaeed.2714@gmail.com>
2023-11-19 12:04:44 +0500

    Changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,23 +1,12 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://elections:yHyBu3lx9HBi4wBc@cluster0.ul85luw.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-const dbConnect = async () => {
+const dbConnect = () => {
   try {
-    await client.connect();
-    await client.db("elections").command({ ping: 1 });
+    const connection = mongoose.connect(process.env.MONGODB_URL);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  } finally {
-    await client.close();
+    console.log("Server is not connected");
   }
 };
+
 module.exports = dbConnect;
