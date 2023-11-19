5f4a8a4ed70ba6a978ccd5f4492c8b56d9f7227e
Hamza Sharif Butt <Hamzasaeed.2714@gmail.com>
2023-11-19 11:48:19 +0500

    Changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,37 +1,23 @@
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-//   "mongodb+srv://elections:yHyBu3lx9HBi4wBc@cluster0.ul85luw.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://elections:yHyBu3lx9HBi4wBc@cluster0.ul85luw.mongodb.net/?retryWrites=true&w=majority";
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// const dbConnect = async () => {
-//   try {
-//     await client.connect();
-//     await client.db("elections").command({ ping: 1 });
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   } finally {
-//     await client.close();
-//   }
-// };
-// module.exports = dbConnect;
-
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    const connection = mongoose.connect(process.env.MONGODB_URL);
-    console.log("Database connected");
+    await client.connect();
+    await client.db("elections").command({ ping: 1 });
   } catch (error) {
-    console.log("Server is not connected");
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    await client.close();
   }
 };
-
 module.exports = dbConnect;
